309fa5ad996bd87688e7b623d5e7a94838d67d7b
luchongbin <chongbin_lu@163.com>
2018-11-09 11:37:49 +0800

    第一次上传 委婉待续

---
+++ app/src/main/java/com/luchongbin/multithreaddownload/multithreaddownload/MainActivity.java
@@ -0,0 +1,13 @@
+package com.luchongbin.multithreaddownload.multithreaddownload;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
